8aa753b711ad9691a34350d24d598a44395a3d70
Darshna <darshnakhatri989@gmail.com>
2023-06-18 00:45:54 +0530

    submitting code-Sun Jun 18 2023 00:45:52 GMT+0530 (India Standard Time)

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,17 +1,13 @@
 import React, {useState} from "react";
 
-const Child = ({handleInput}) => {
-    return (
-       <div className="child">
-          <h1>Child Component</h1>
-          <input
-             type="text"
-             onChange={(e) => {
-                handleInput(e.target.value);
-             }}
-          />
-       </div>
-    );
+const Child = ({renderFun})=>{
+
+    return(
+        <div className="child">
+            <h1>Child Component</h1>
+            <input type="text" onChange={renderFun}/>
+        </div>
+    )
 }
 
-export default Child
+export default Child;
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,16 +1,20 @@
 import React, {useState} from "react";
-import Child from Child;
+import Child from "./child";
 
-const Parent = ()=> {
-    let [inputValue, setInputValue] = useState("")
+const Parent = ()=>{
+    let [change, setChange] = useState('');
+
+    function renderChange(e){
+        setChange(e.target.value);
+    }
 
     return(
         <div className="parent">
             <h1>Parent Component</h1>
-            <p>{inputValue}</p>
-            <Child handleInput = {setInputValue} />
+            <p>{change}</p>
+            <Child renderFun={renderChange} />
         </div>
     )
 }
 
-export default Parent
+export default Parent;
